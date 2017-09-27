5b498d4cad35140b1b0072cd8abbfce8f97da72c
Selçuk İTMİŞ <slck87@hotmail.com>
2017-09-28 01:44:38 +0300

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,27 @@
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
+  if (err) {
+    console.log("Unable to connect to MongoDb server");
+  }
+  console.log("Connected to MongoDb server");
+
+//   db.collection("Todos").insertOne({
+//     text: "Some text",
+//     completed: false
+//   }, (err, result) => {
+//     if (err) console.log("unable to insert todo");
+//     console.log(JSON.stringify(result.ops));
+//   });
+
+//   db.collection("Users").insertOne({
+//     name: "Selçuk İTMİŞ",
+//     age: 29,
+//     location:"İstanbul"
+//   }, (err, result) => {
+//     if (err) console.log("unable to insert member");
+//     console.log(JSON.stringify(result.ops));
+//   });
+
+  db.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,27 @@
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
+  if (err) {
+    console.log("Unable to connect to MongoDb server");
+  }
+  console.log("Connected to MongoDb server");
+
+  db
+    .collection("Users")
+    .find({
+      //_id: new ObjectId("59cc248124d9d56ccb88609b")
+    })
+    .toArray()
+    .then(
+      docs => {
+        console.log("todos");
+        console.log(JSON.stringify(docs, undefined, 2));
+      },
+      err => {
+        console.log("We have error: ", err);
+      }
+    );
+
+  db.close();
+});
