75d1144f539a7295c5dbbbcfa72b5e40042bce2d
Selçuk İTMİŞ <slck87@hotmail.com>
2017-10-01 01:08:25 +0300

    mongo config

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,6 +1,8 @@
 const { MongoClient, ObjectID } = require("mongodb").MongoClient;
 
-let mongoUrl = process.argv.MONGODB_URI || "mongodb://localhost:27017/TodoApp";
+let mongoUrl = process.argv.MONGODB_URI
+  ? "mongodb://<dba>:<123123>@ds157624.mlab.com:57624/heroku_68nrvdz3"
+  : "mongodb://localhost:27017/TodoApp";
 
 MongoClient.connect(mongoUrl, (err, db) => {
   if (err) {
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,7 @@
 let mongoose = require("mongoose");
-let mongoUrl = process.argv.MONGODB_URI || "mongodb://localhost:27017/TodoApp";
+let mongoUrl = process.argv.MONGODB_URI
+  ? "mongodb://<dba>:<123123>@ds157624.mlab.com:57624/heroku_68nrvdz3"
+  : "mongodb://localhost:27017/TodoApp";
 mongoose.Promise = global.Promise;
 
 mongoose.connect(mongoUrl, { useMongoClient: true });
