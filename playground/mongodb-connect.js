4125bdab012b24b9583437c14f5dfa921d4f5eaa
Selçuk İTMİŞ <slck87@hotmail.com>
2017-10-01 01:20:25 +0300

    config setting

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,11 +1,6 @@
 const { MongoClient, ObjectID } = require("mongodb").MongoClient;
 
-let mongoUrl =
-  "mongodb://heroku_68nrvdz3:7457hcjjfb641aatrt6bv0c4ko@ds157624.mlab.com:57624/heroku_68nrvdz3";
-
-// let mongoUrl = process.argv.MONGODB_URI
-// ? "mongodb://<dba>:<123123>@ds157624.mlab.com:57624/heroku_68nrvdz3"
-// : "mongodb://localhost:27017/TodoApp";
+let mongoUrl = process.argv.MONGODB_URI || "mongodb://localhost:27017/TodoApp";
 
 MongoClient.connect(mongoUrl, (err, db) => {
   if (err) {
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,6 @@
 let mongoose = require("mongoose");
 
-let mongoUrl =
-"mongodb://heroku_68nrvdz3:7457hcjjfb641aatrt6bv0c4ko@ds157624.mlab.com:57624/heroku_68nrvdz3";
-
-// let mongoUrl = process.argv.MONGODB_URI
-// ? "mongodb://<dba>:<123123>@ds157624.mlab.com:57624/heroku_68nrvdz3"
-// : "mongodb://localhost:27017/TodoApp";
+let mongoUrl = process.argv.MONGODB_URI || "mongodb://localhost:27017/TodoApp";
 
 mongoose.Promise = global.Promise;
 
