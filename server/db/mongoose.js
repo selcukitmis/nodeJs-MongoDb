675023205fdcbc323cd0c299f17c2fdf35759749
Selçuk İTMİŞ <slck87@hotmail.com>
2017-10-01 01:16:11 +0300

    mongo config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 
 let mongoUrl =
-"mongodb://<dba>:<123123>@ds157624.mlab.com:57624/heroku_68nrvdz3";
+"mongodb://heroku_68nrvdz3:7457hcjjfb641aatrt6bv0c4ko@ds157624.mlab.com:57624/heroku_68nrvdz3";
 
 // let mongoUrl = process.argv.MONGODB_URI
 // ? "mongodb://<dba>:<123123>@ds157624.mlab.com:57624/heroku_68nrvdz3"
