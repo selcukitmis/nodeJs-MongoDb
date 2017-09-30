b33159364ec65fd5886d0b265a0ee69fe1edcc39
Selçuk İTMİŞ <slck87@hotmail.com>
2017-10-01 02:57:47 +0300

    config added

--- server/server.js
+++ server/server.js
@@ -1,11 +1,4 @@
-let env = process.env.NODE_ENV || "development";
-if (env === "development") {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp";
-} else if (env === "test") {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest";
-}
+require("./config/config");
 let _ = require("lodash");
 let express = require("express");
 let bodyParser = require("body-parser");
