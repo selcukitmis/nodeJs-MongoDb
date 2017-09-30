6a8343ef6503be571c34197328439a86e0adf2d3
Selçuk İTMİŞ <slck87@hotmail.com>
2017-10-01 00:53:57 +0300

    change settings

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,30 +1,29 @@
 const { MongoClient, ObjectID } = require("mongodb").MongoClient;
 
-MongoClient.connect(
-  process.argv.MONGODB_URI || "mongodb://localhost:27017/TodoApp",
-  (err, db) => {
-    if (err) {
-      console.log("Unable to connect to MongoDb server");
-    }
-    console.log("Connected to MongoDb server");
+let mongoUrl = process.argv.MONGODB_URI || "mongodb://localhost:27017/TodoApp";
 
-    //   db.collection("Todos").insertOne({
-    //     text: "Some text",
-    //     completed: false
-    //   }, (err, result) => {
-    //     if (err) console.log("unable to insert todo");
-    //     console.log(JSON.stringify(result.ops));
-    //   });
-
-    //   db.collection("Users").insertOne({
-    //     name: "Selçuk İTMİŞ",
-    //     age: 29,
-    //     location:"İstanbul"
-    //   }, (err, result) => {
-    //     if (err) console.log("unable to insert member");
-    //     console.log(JSON.stringify(result.ops));
-    //   });
-
-    db.close();
+MongoClient.connect(mongoUrl, (err, db) => {
+  if (err) {
+    console.log("Unable to connect to MongoDb server");
   }
-);
+  console.log("Connected to MongoDb server");
+
+  //   db.collection("Todos").insertOne({
+  //     text: "Some text",
+  //     completed: false
+  //   }, (err, result) => {
+  //     if (err) console.log("unable to insert todo");
+  //     console.log(JSON.stringify(result.ops));
+  //   });
+
+  //   db.collection("Users").insertOne({
+  //     name: "Selçuk İTMİŞ",
+  //     age: 29,
+  //     location:"İstanbul"
+  //   }, (err, result) => {
+  //     if (err) console.log("unable to insert member");
+  //     console.log(JSON.stringify(result.ops));
+  //   });
+
+  db.close();
+});
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,8 @@
 let mongoose = require("mongoose");
-
+let mongoUrl = process.argv.MONGODB_URI || "mongodb://localhost:27017/TodoApp";
 mongoose.Promise = global.Promise;
 
-mongoose.connect(
-  process.argv.MONGODB_URI || "mongodb://localhost:27017/TodoApp",
-  { useMongoClient: true }
-);
+mongoose.connect(mongoUrl, { useMongoClient: true });
 
 module.exports = {
   mongoose
