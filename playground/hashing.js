ca8d4a6bc31677bf1a64d24240cd70485c1d95fa
Selçuk İTMİŞ <slck87@hotmail.com>
2017-10-04 01:50:40 +0300

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -15,15 +15,29 @@ const { SHA256 } = require("crypto-js");
 //   hash: SHA256(JSON.stringify(data) + "somestring").toString()
 // };
 
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
-var jwtData = {
-  id: 10
-};
+// var jwtData = {
+//   id: 10
+// };
 
-var sign = jwt.sign(jwtData, "1234");
+// var sign = jwt.sign(jwtData, "1234");
 
-var verify = jwt.verify(sign, "1234");
+// var verify = jwt.verify(sign, "1234");
 
-console.log(sign);
-console.log(verify);
+// console.log(sign);
+// console.log(verify);
+
+const bcrypt = require("bcryptjs");
+let password = "abcsss";
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+let hashedPass = "$2a$10$yvMpWLxf6tiwztTDQQPFdOOVXlOex7V7Ix.k5jYWtpQ1mDL9H0.ia";
+bcrypt.compare(password, hashedPass, (err, res) => {
+  console.log(res);
+});
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const _ = require("lodash");
+const bcrypt = require("bcryptjs");
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -36,6 +37,25 @@ var UserSchema = new mongoose.Schema({
   ]
 });
 
+// sadece belli parametreleri return etmek istersek kullanıyoruz!!!!
+UserSchema.methods.toJSON = function() {
+  var user = this;
+  var userObject = user.toObject();
+  return _.pick(userObject, ["_id", "email"]);
+};
+
+UserSchema.methods.generateAuthToken = function() {
+  var user = this;
+  var access = "auth";
+  var token = jwt
+    .sign({ _id: user._id.toHexString(), access }, "abc123!")
+    .toString();
+  user.tokens.push({ access, token });
+  return user.save().then(() => {
+    return token;
+  });
+};
+
 UserSchema.statics.findByToken = function(token) {
   let user = this;
   let decoded;
@@ -55,25 +75,19 @@ UserSchema.statics.findByToken = function(token) {
   });
 };
 
-// sadece belli parametreleri return etmek istersek kullanıyoruz!!!!
-UserSchema.methods.toJSON = function() {
-  var user = this;
-  var userObject = user.toObject();
-  return _.pick(userObject, ["_id", "email"]);
-};
-
-UserSchema.methods.generateAuthToken = function() {
-  console.log("at here");
-  var user = this;
-  var access = "auth";
-  var token = jwt
-    .sign({ _id: user._id.toHexString(), access }, "abc123!")
-    .toString();
-  user.tokens.push({ access, token });
-  return user.save().then(() => {
-    return token;
-  });
-};
+UserSchema.pre("save", function(next) {
+  let user = this;
+  if (user.isModified("password")) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model("User", UserSchema);
 
